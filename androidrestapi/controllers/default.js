d944a7fc6db5bd9c095d1253f58af4851d164c23
loyyo <57000021+loyyo@users.noreply.github.com>
2023-01-05 13:46:04 +0100

    images compression

--- androidrestapi/controllers/default.js
+++ androidrestapi/controllers/default.js
@@ -1,8 +1,12 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { jsPDF } from 'jspdf';
-import 'jspdf-autotable';
 import fs from 'fs';
+import imagemin from 'imagemin';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminOptipng from 'imagemin-optipng';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminMozjpeg from 'imagemin-mozjpeg';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename).replace('controllers', '');
@@ -16,25 +20,50 @@ export const uploadImages = (req, res) => {
 	let y_offset = 0;
 	let x_offset = 0;
 	let index = 0;
-	let bannerImage = fs.readFileSync(__dirname + `banners\\${banner}.jpg`);
+
+	// (async () => {
+	// 	await imagemin(['banners/*.jpg'], {
+	// 		destination: 'build/banners',
+	// 		plugins: [imageminJpegtran(), imageminMozjpeg({ quality: 75 })],
+	// 	});
+	// 	console.log('images optimized');
+	// })();
+
+	let bannerImage = fs.readFileSync(__dirname + `build\\banners\\${banner}.jpg`);
+
 	doc.addImage(bannerImage, 'JPEG', 90, 0, 15, 297, 'banner', 'FAST');
 	doc.addImage(bannerImage, 'JPEG', 195, 0, 15, 297, 'banner', 'FAST');
 
 	Object.keys(files).forEach((key) => {
-		index++;
-		if (index == 4) {
-			x_offset += 105;
-			y_offset = 0;
-		}
-
 		const filepath = path.join(__dirname, 'data', senderName, files[key].name);
-		doc.addImage(files[key].data, 'JPEG', x_offset, y_offset, 90, 99, undefined, 'FAST');
-		y_offset += 99;
 		files[key].mv(filepath, (err) => {
 			if (err) return res.status(500).json({ status: '500 error', message: err });
 		});
 	});
 
-	doc.save(`data/${senderName}/table.pdf`);
-	res.json({ status: 'success', message: Object.keys(files).toString() });
+	(async () => {
+		await imagemin([`data/${senderName}/*.{jpg,jpeg,png}`], {
+			destination: `build/${senderName}`,
+			plugins: [
+				imageminJpegtran(),
+				imageminMozjpeg({ quality: 75 }),
+				imageminPngquant(),
+				imageminOptipng(),
+			],
+		});
+		Object.keys(files).forEach((key) => {
+			let fileName = files[key].name;
+			let file = fs.readFileSync(`${__dirname}build\\${senderName}\\${fileName}`);
+			index++;
+			if (index == 4) {
+				x_offset += 105;
+				y_offset = 0;
+			}
+			doc.addImage(file, 'JPEG', x_offset, y_offset, 90, 99, undefined, 'FAST');
+			y_offset += 99;
+		});
+		fs.rmSync(`${__dirname}build\\${senderName}`, { recursive: true, force: true });
+		doc.save(`data/${senderName}/table.pdf`);
+		res.json({ status: 'success', message: Object.keys(files).toString() });
+	})();
 };
