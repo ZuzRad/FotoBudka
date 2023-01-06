f1ed7c8b1b39a20fb7797f86de10bd4f2b149cfd
H A L T <72382258+Kamil097@users.noreply.github.com>
2023-01-06 18:01:25 +0100

    MainPage

--- app/src/main/java/com/example/fotozabawa/MainActivity.kt
+++ app/src/main/java/com/example/fotozabawa/MainActivity.kt
@@ -5,6 +5,7 @@ import android.Manifest.permission.CAMERA
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.R
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
 import android.hardware.camera2.*
 import android.net.Uri
@@ -14,17 +15,30 @@ import android.os.Environment
 import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
 import android.provider.MediaStore
+import android.view.MenuItem
 import android.view.Surface
 import android.view.TextureView
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import java.io.File
 import java.io.IOException
 import java.util.*
 
 
+
 class MainActivity : AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.fotozabawa.R.layout.fragment_strona_glowna)
+
+    }
+}
 //    //----------------------------wersja 1 klikając start pojawi się kamera w tle (jeszcze nie robi zdjęć)--------------------------------
 //    private var myCameraCaptureSession: CameraCaptureSession? = null
 //    private var myCameraID: String? = null
@@ -146,4 +160,4 @@ class MainActivity : AppCompatActivity() {
 //        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
 //        startActivity(cameraIntent)
 //    }
-}
+//}
