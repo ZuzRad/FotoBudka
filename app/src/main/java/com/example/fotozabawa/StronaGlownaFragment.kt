4ef85c7995af1d0fbe9ebb2e1c3dfa784f0a2996
H A L T <72382258+Kamil097@users.noreply.github.com>
2023-01-06 18:21:05 +0100

    another push

--- app/src/main/java/com/example/fotozabawa/StronaGlownaFragment.kt
+++ app/src/main/java/com/example/fotozabawa/StronaGlownaFragment.kt
@@ -5,12 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.example.fotozabawa.databinding.FragmentStronaGlownaBinding
+
 
 class StronaGlownaFragment : Fragment() {
+    private var _binding: FragmentStronaGlownaBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentStronaGlownaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val myButton = view.findViewById<Button>(R.id.button_menu)
+        myButton.setOnClickListener{
+            val fragment : Fragment = MenuFragment()
+            val fragmentManager = requireActivity().supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frameLayout, fragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+            requireActivity().title = "Menu"
+        }
     }
 
 }
