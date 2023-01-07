db23f52a9b0935395e7a4d210575115ff5475501
H A L T <72382258+Kamil097@users.noreply.github.com>
2023-01-07 15:55:51 +0100

    cykanie

--- app/src/main/java/com/example/fotozabawa/StronaGlownaFragment.kt
+++ app/src/main/java/com/example/fotozabawa/StronaGlownaFragment.kt
@@ -1,17 +1,27 @@
 package com.example.fotozabawa
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Context.CAMERA_SERVICE
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
 import android.hardware.camera2.*
+import android.media.ImageReader
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
+import android.provider.MediaStore
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.getSystemService
 import com.example.fotozabawa.databinding.FragmentStronaGlownaBinding
+import java.io.FileOutputStream
+import java.nio.ByteBuffer
 import java.util.*
 
 
@@ -26,6 +36,8 @@ class StronaGlownaFragment : Fragment() {
     private var myCaptureRequestBuilder: CaptureRequest.Builder? = null
 
 
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         _binding = FragmentStronaGlownaBinding.inflate(inflater, container, false)
@@ -34,7 +46,7 @@ class StronaGlownaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+         val texture = view.findViewById<TextureView>(R.id.textureView)
         myTextrureView = view.findViewById(R.id.textureView)
         myCameraManager = requireActivity().getSystemService(CAMERA_SERVICE) as CameraManager
         openCamera()
@@ -51,7 +63,35 @@ class StronaGlownaFragment : Fragment() {
             requireActivity().title = "Menu"
         }
         val buttonStart = view.findViewById<Button>(R.id.button_start)
+
         buttonStart.setOnClickListener {
+            val bitmap = texture.getBitmap()
+            val directory = android.os.Environment.DIRECTORY_DCIM
+            val toast = Toast.makeText(activity, directory.toString(), Toast.LENGTH_SHORT)
+            val imageReader = ImageReader.newInstance(640, 480, ImageFormat.JPEG, 2)
+            imageReader.setOnImageAvailableListener({ reader ->
+                val image = reader.acquireLatestImage()
+                val planes = image.planes
+                val buffer = planes[0].buffer
+                val bytes = ByteBuffer.allocate(buffer.capacity()).put(buffer).array()
+                val byteArray = bytes
+
+                // Save the byte[] to a file and insert it into the media store...
+
+            val myoutput = FileOutputStream(directory)
+            bitmap!!.compress(Bitmap.CompressFormat.JPEG, 100, myoutput)
+            myoutput.close()
+
+            val values = ContentValues().apply {
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000)
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+            }
+            val uri = requireContext().contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+            val outputStream = requireContext().contentResolver.openOutputStream(uri!!)
+            outputStream!!.write(byteArray)
+            outputStream.close()
+            }, null)
 
         }
     }
@@ -122,126 +162,3 @@ class StronaGlownaFragment : Fragment() {
 
 
 }
-
-//    //----------------------------wersja 1 klikając start pojawi się kamera w tle (jeszcze nie robi zdjęć)--------------------------------
-//    private var myCameraCaptureSession: CameraCaptureSession? = null
-//    private var myCameraID: String? = null
-//    private var myCameraManager: CameraManager? = null
-//    private var myCameraDevice: CameraDevice? = null
-//    private var myTextrureView: TextureView? = null
-//    private var myCaptureRequestBuilder: CaptureRequest.Builder? = null
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(com.example.fotozabawa.R.layout.activity_main)
-//        myTextrureView = findViewById(com.example.fotozabawa.R.id.textureView)
-//        myCameraManager = getSystemService(CAMERA_SERVICE) as CameraManager
-//        openCamera()
-//        //cameraPreview()
-//    }
-//
-//    private val myStateCallBack: CameraDevice.StateCallback =
-//        object : CameraDevice.StateCallback() {
-//            override fun onOpened(camera: CameraDevice) {
-//                myCameraDevice = camera
-//            }
-//
-//            override fun onDisconnected(camera: CameraDevice) {
-//                myCameraDevice!!.close()
-//            }
-//
-//            override fun onError(camera: CameraDevice, error: Int) {
-//                myCameraDevice!!.close()
-//                myCameraDevice = null
-//            }
-//        }
-//
-//    private fun openCamera() {
-//        try {
-//            myCameraID = myCameraManager!!.cameraIdList[0]
-//            val CameraID = myCameraID
-//            ActivityCompat.requestPermissions(
-//                this@MainActivity,
-//                arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE),
-//                PackageManager.PERMISSION_GRANTED
-//            )
-//            if (ActivityCompat.checkSelfPermission(
-//                    this,
-//                    Manifest.permission.CAMERA
-//                ) != PackageManager.PERMISSION_GRANTED
-//            ) {
-//                return
-//            }
-//            if (CameraID != null) {
-//                myCameraManager!!.openCamera(CameraID, myStateCallBack, null)
-//            }
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//        }
-//    }
-//
-//    fun cameraPreview(view: View?) {
-//        val mySurfaceTexture = myTextrureView!!.surfaceTexture
-//        val mySurface = Surface(mySurfaceTexture)
-//        try {
-//            myCaptureRequestBuilder =
-//                myCameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-//            myCaptureRequestBuilder!!.addTarget(mySurface)
-//            myCameraDevice!!.createCaptureSession(
-//                Arrays.asList(mySurface), object : CameraCaptureSession.StateCallback() {
-//                    override fun onConfigured(session: CameraCaptureSession) {
-//                        myCameraCaptureSession = session
-//                        myCaptureRequestBuilder!!.set(
-//                            CaptureRequest.CONTROL_MODE,
-//                            CameraMetadata.CONTROL_MODE_AUTO
-//                        )
-//                        try {
-//                            myCameraCaptureSession!!.setRepeatingRequest(
-//                                myCaptureRequestBuilder!!.build(),
-//                                null,
-//                                null
-//                            )
-//                        } catch (e: CameraAccessException) {
-//                            e.printStackTrace()
-//                        }
-//                    }
-//
-//                    override fun onConfigureFailed(session: CameraCaptureSession) {}
-//                }, null
-//            )
-//        } catch (e: CameraAccessException) {
-//            e.printStackTrace()
-//        }
-//    }
-
-//    //-----------------------------------inna wersja robienia zdjęć(należy dodać w acivity_main funkcje CameraButton na onclicku przycisku)----------------------------------
-//    var index = 0
-//    val directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-//        .toString() + "/myCamera/"
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(com.example.fotozabawa.R.layout.activity_main)
-//        ActivityCompat.requestPermissions(
-//            this,
-//            arrayOf(CAMERA, WRITE_EXTERNAL_STORAGE),
-//            PackageManager.PERMISSION_GRANTED
-//        )
-//        val builder = VmPolicy.Builder()
-//        StrictMode.setVmPolicy(builder.build())
-//    }
-//
-//    fun CameraButton(view: View?) {
-//        index++
-//        val file = "$directory$index.jpg"
-//        val newFile = File(file)
-//        try {
-//            newFile.createNewFile()
-//        } catch (e: IOException) {
-//            e.printStackTrace()
-//        }
-//        val outputFileUri: Uri = Uri.fromFile(newFile)
-//        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri)
-//        startActivity(cameraIntent)
-//    }
-//}
