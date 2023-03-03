0e5bd997a4c49ba605e7ff9db5b89a70d4763646
ZuzRad <zuzarad785@student.polsl.pl>
2023-03-04 00:13:25 +0100

    first

--- androidrestapi/middleware/filesPayloadExists.js
+++
@@ -1,4 +0,0 @@
-export const filesPayloadExists = (req, res, next) => {
-	if (!req.files) return res.status(400).json({ status: '400 error', message: 'Missing files' });
-	next();
-};
--- app/src/main/java/com/example/fotozabawa/upload/UploadResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fotozabawa.upload
-
-data class UploadResponse(
-    val status: String,
-    val message: String
-)
